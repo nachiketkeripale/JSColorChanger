66d46b67fb1e367b63a1510a2a579185cfc6c4ad
nachiketkeripale <keripalenachiket@gmail.com>
2022-11-15 10:41:11 +0530

    Pushing code to GitHub.com

---
+++ app.js
@@ -0,0 +1,10 @@
+//used getElementByid to get the button.
+const btn = document.getElementById("button");
+
+// added a event listener on the button and changed the style of canvas to green.
+btn.addEventListener("click",onclick = () =>{
+
+    document.getElementById("canvas").style.backgroundColor = "green";
+
+});
+
